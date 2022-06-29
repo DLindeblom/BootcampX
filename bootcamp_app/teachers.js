7d72d28d046d8fb86fa16030c2939891d2f09c0b
DLindeblom <drdlindeblom@gmail.com>
2022-06-29 20:37:12 +0000

    updated files to include sql injection protection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-  SELECT DISTINCT teachers.name as name, cohorts.name as cohort
-  FROM teachers
-  JOIN assistance_requests ON teacher_id = teachers.id
-  JOIN students ON students.id = student_id
-  JOIN cohorts ON cohort_id = cohorts.id
-  Where cohorts.name LIKE '${process.argv[2]}'
-  Order by name;
-  `)
+const queryString = `
+SELECT DISTINCT teachers.name as name, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohort_id = cohorts.id
+Where cohorts.name LIKE $1
+Order by name;
+`;
+const cohortName = process.argv[2]
+const values = [`%${cohortName}%`]
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
-      console.log(`${user.cohort}: ${user.name}`)
-    })
-  })
+      console.log(`${user.cohort}: ${user.name}`);
+    });
+  });
